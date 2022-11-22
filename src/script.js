91737435c376fe872ef7bd3b125318f393898b6d
paulopariz <paulopariz01@gmail.com>
2022-11-21 22:19:16 -0300

    responsive medium/large

--- src/script.js
+++ src/script.js
@@ -1,13 +1,33 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: 0,
     spaceBetween: 30,
-    slidesPerGroup: 3,
+    slidesPerGroup: 1,
     loop: false,
   
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints:{
+      320:{
+        slidesPerView: 1,
+      },
+      480:{
+        slidesPerView: 1.5,
+      },
+      530:{
+        slidesPerView: 1.70
+      },
+      640:{
+        slidesPerView: 2
+      },
+      1050:{
+        slidesPerView: 2.5
+      },
+      1220:{
+        slidesPerView: 3,
+      }
+    }
   });
 
 //VER MAIS
